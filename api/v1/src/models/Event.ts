bb86ba145826ffc2912ba5aa6b52929e4dbf7359
jorgechauxjr <chauxjorge@hotmail.com>
2021-04-15 14:08:19 -0500

    [Update]new attributes in Event Schema

--- api/v1/src/models/Event.ts
+++ api/v1/src/models/Event.ts
@@ -10,7 +10,40 @@ const eventSchema = new Schema({
     type: String,
     required: true,
     trim: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    trim: true
+  },
+  duration: {
+    length: {
+      type: Number,
+      required: true,
+    },
+    format: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: ['days', 'hours']
+    }
+  },
+  onSite: {
+    type: Boolean,
+    required: true,
+  },
+  venue: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
   }
+  // FALTA EL USUARIO QUE CREA EL EVENTO
+
+  // FALTA AGREGAR ARREGLO USUARIOS QUE VAN A ASISTIR
 }, {
   versionKey: false,
   timestamps: true
