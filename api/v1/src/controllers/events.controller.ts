bf51e76d0b1e4b6bb42aba0dbd4891fafd0e17d2
Felipe Satizabal <1165@holbertonschool.com>
2021-04-16 12:41:19 -0500

    [update] improving handle errors in event controller and new tests

--- api/v1/src/controllers/events.controller.ts
+++ api/v1/src/controllers/events.controller.ts
@@ -1,18 +1,18 @@
 import { RequestHandler } from 'express';
 import Event from '../models/Event';
 
+/* FUNCTION TO CREATE EVENT */
 export const createEvent: RequestHandler = async (req, res) => {
-  /* PARA LOS USUARIOS
-    const videosFound = await Video.find({url: req.body.url});
-    if (videosFound)
-      res.status(301).json({message: 'mensaje de error'});
-   */
-
-  const event = new Event(req.body);
-  const savedEvent = await event.save();
-  res.json(savedEvent);
+  try {
+    const event = new Event(req.body);
+    const savedEvent = await event.save();
+    return res.json(savedEvent);
+  } catch(error) {
+    return res.status(301).json('All fields are required');
+  }
 };
 
+/* FUNCTION TO GET ALL EVENTS */
 export const getEvents: RequestHandler = async (req, res) => {
   try {
     const events = await Event.find();
@@ -22,20 +22,35 @@ export const getEvents: RequestHandler = async (req, res) => {
   }
 };
 
+/* FUNCTION TO GET AN EVENT BY ID */
 export const getEvent: RequestHandler = async (req, res) => {
-  const event = await Event.findById(req.params.id);
-  if (!event) return res.status(204).json(); // no sale mensaje para el error 204
-  res.json(event);
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) return res.status(204).json(); // no sale mensaje para el error 204
+    return res.json(event);
+  } catch(error) {
+    return res.status(204).json();
+  }
 };
 
+/* FUNCTION TO DELETE AN EXISTING EVENT */
 export const deleteEvent: RequestHandler = async (req, res) => {
-  const event = await Event.findByIdAndDelete(req.params.id);
-  if (!event) return res.status(204).json(); // no sale mensaje para el error 204
-  res.json(event);
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event) return res.status(204).json(); // no sale mensaje para el error 204
+    res.json(event);
+  } catch(error) {
+    return res.status(204).json();
+  }
 };
 
+/* FUNCTION TO UPDATE AN EVENT BY ID */
 export const updateEvent: RequestHandler = async (req, res) => {
-  const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  if (!event) return res.status(204).json(); // no sale mensaje para el error 204
-  res.json(event);
+  try {
+    const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!event) return res.status(204).json(); // no sale mensaje para el error 204
+    res.json(event);
+  } catch(error) {
+    return res.status(204).json();
+  }
 };
