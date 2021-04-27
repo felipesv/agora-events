c665a3678d1c95fa438c81cc6ebc7d9acf83e462
Felipe Satizabal <1165@holbertonschool.com>
2021-04-27 12:04:16 -0500

    [add] base and initial files frontend

---
+++ frontend/config/webpack.config.dev.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const Dotenv = require('dotenv-webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].bundle.js",
+    assetModuleFilename: 'assets/[hash].[ext]',
+    path: path.resolve(__dirname, "../dist"),
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    historyApiFallback: true,
+    port: 8564,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      // favicon: './public/favicon.ico'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/mystyles.css'
+    }),
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin(),
+//    new Dotenv()
+  ],
+  resolve: {
+    extensions: ['.js' , '.jsx'],
+    alias: {
+      '@assets': path.resolve(__dirname, '../src/assets/'),
+      '@components': path.resolve(__dirname, '../src/components/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|ico)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App/App';
+import './index.css';
+import './mystyles.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <div>holi</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
